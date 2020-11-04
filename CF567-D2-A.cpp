c5d50adaad41edc07c158747f3217939c2f3e9a6
pro-GRMR <73429648+pro-GRMR@users.noreply.github.com>
2020-11-04 11:34:56 +0600

    Update CF567-D2-A.cpp

--- CF567-D2-A.cpp
+++ CF567-D2-A.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int main()
 {
     int n, x;
     cin >> n;
-    long long int a[n], b[n], c[3*n], mx, mn;
+    int a[n];
     for(int i=0; i<n;i++){
         cin >> a[i];
     }
-    for(int i=0;i<n;i++){
-        mx=max(abs(a[i]-a[n-1]),abs(a[i]-a[0]));
-        if(i!=0 && i!=n-1){
-            mn=min(abs(a[i]-a[i-1]),abs(a[i]-a[i+1]));
-        }
-        else if(i==0){
-            mn=abs(a[i]-a[i+1]);
-        }
-        else{
-            mn=abs(a[i]-a[i-1]);
-        }
+    long long int mn=a[1]-a[0], mx=a[n-1]-a[0];
+    cout << mn << " " << mx << endl;
+    for(int i=1;i<n-1;i++){
+        mx=max(a[n-1]-a[i],a[i]-a[0]);
+        mn=min(a[i]-a[i-1], a[i+1]-a[i]);
         cout << mn << " " << mx << endl;
     }
+    cout << a[n-1]-a[n-2] << " " << a[n-1]-a[0] << endl;
     return 0;
 }
