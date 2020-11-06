8590aae97a12cfd627c04665c341c869a3aeba6d
pro-GRMR <73429648+pro-GRMR@users.noreply.github.com>
2020-11-06 13:05:53 +0600

    Create CF742-D2-A.cpp

---
+++ CF742-D2-A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if(n==0){
+        cout << 1 << endl;
+        return 0;
+    }
+    switch(n%4){
+    case 0:
+        cout << 6 << endl;
+        break;
+    case 1:
+        cout << 8 << endl;
+        break;
+    case 2:
+        cout << 4 << endl;
+        break;
+    default:
+        cout << 2 << endl;
+    }
+    return 0;
+}
